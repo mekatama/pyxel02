d10d1b99eda6a54b1cbfb48c84dad1725402ac7c
mekatama <tamatte@hotmail.com>
2022-12-28 19:03:49 +0900

    test
    vscde動く確認用

---
+++ game01.py
@@ -0,0 +1,11 @@
+import pyxel
+ 
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        pass
+    def draw(self):
+        pass
+App()
