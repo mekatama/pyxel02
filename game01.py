064549d9c5bd54ab2b392ad1693266718a381f82
mekatama <tamatte@hotmail.com>
2023-01-04 14:18:26 +0900

    player停止処理
    Aボタン入力で

--- game01.py
+++ game01.py
@@ -10,21 +10,32 @@ class App:
         self.x = 0
         #playerの移動方向flag
         self.isRight = True
+        #playerの停止flag
+        self.isStop = False
         #実行開始 更新関数 描画関数
         pyxel.run(self.update, self.draw)
 
 	#更新関数
     def update(self):
+        #key入力(player停止)
+        if pyxel.btn(pyxel.KEY_A):
+            #押したら
+            self.isStop = True
+        elif pyxel.btnr(pyxel.KEY_A):
+            #離したら
+            self.isStop = False
+
         #playerの移動方向判定
         if self.x <= 0:
             self.isRight = True
         elif self.x >= 104:
             self.isRight = False
 	    #playerの往復移動
-        if self.isRight == True:
-            self.x = (self.x + 1)
-        elif self.isRight == False:
-            self.x = (self.x - 1)
+        if self.isStop == False:
+            if self.isRight == True:
+                self.x = (self.x + 1)
+            elif self.isRight == False:
+                self.x = (self.x - 1)
         pass
 
 	#描画関数
