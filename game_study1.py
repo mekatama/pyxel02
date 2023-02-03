3d4343453c5a72c15f426e8f8cc820165f81c4f7
mekatama <tamatte@hotmail.com>
2023-02-03 18:26:17 +0900

    playerの向きをベクトル制御

--- game_study1.py
+++ game_study1.py
@@ -13,10 +13,12 @@ class player:
     def __init__(self):
         #Vec2クラス内でself.x=0 self.y=0と処理されてるっぽい
         self.pos = Vec2(0, 0)
+        self.vec = 0    #向き(ベクトル)
 
-    def update(self, x, y):
+    def update(self, x, y, dx):
         self.pos.x = x
         self.pos.y = y
+        self.vec = dx
 
 #ゲーム管理
 class App:
@@ -38,7 +40,13 @@ class App:
             pyxel.quit()
         
         #Playerクラス更新
-        self.mPlayer.update(pyxel.mouse_x, pyxel.mouse_y)
+        dx = pyxel.mouse_x - self.mPlayer.pos.x #x軸方向の移動量
+        dy = pyxel.mouse_y - self.mPlayer.pos.y #y軸方向の移動量
+        #移動量判定
+        if dx != 0:
+            self.mPlayer.update(pyxel.mouse_x, pyxel.mouse_y, dx)   #移動と向き変更
+        elif dy != 0:   #真上or真下に移動
+            self.mPlayer.update(pyxel.mouse_x, pyxel.mouse_y, 0)   #移動のみ変更
 
 	#描画
     def draw(self):
@@ -46,8 +54,13 @@ class App:
         pyxel.cls(0)
         #テキスト表示(colorが周期的に変わる)
         pyxel.text(55, 40, "Are you Kururu?", pyxel.frame_count % 16)
-        #editorデータ描画(player)
-        pyxel.blt(self.mPlayer.pos.x, self.mPlayer.pos.y, 0, 0, 0, -16, 16, 0)
+        #player向き判定
+        if self.mPlayer.vec > 0:    #右向き
+            #editorデータ描画(player)
+            pyxel.blt(self.mPlayer.pos.x, self.mPlayer.pos.y, 0, 0, 0, 16, 16, 0)
+        else:                       #左向き
+            pyxel.blt(self.mPlayer.pos.x, self.mPlayer.pos.y, 0, 0, 0, -16, 16, 0)
+
             #描画座標(左上のX座標) #描画座標(左上のY座標)
             #画像番号
             #切り出しの左上のX座標 #切り出しの左上のY座標
