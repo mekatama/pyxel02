cc5946727acc1c840a97477294e0bd90e7d661bc
mekatama <tamatte@hotmail.com>
2023-02-23 10:19:54 +0900

    game pad 対応

--- game03.py
+++ game03.py
@@ -41,7 +41,7 @@ class Player:
         self.isStop = False #playerの停止flag
     def update(self):
         #key入力(player停止)
-        if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_A):
+        if pyxel.btnp(pyxel.KEY_A) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
             self.isStop = True  #押したら
         elif pyxel.btnr(pyxel.KEY_A)  or pyxel.btnr(pyxel.GAMEPAD1_BUTTON_A):
             self.isStop = False #離したら
