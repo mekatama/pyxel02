241ee4abef4fc37eeb9e2c1262a257cede779c51
mekatama <tamatte@hotmail.com>
2023-06-10 16:06:28 +0900

    一時凍結

---
+++ game06.py
@@ -0,0 +1,153 @@
+#ジャンプアクション01
+import pyxel
+#画面遷移用の変数
+SCENE_TITLE = 0	    #タイトル画面
+SCENE_PLAY = 1	    #ゲーム画面
+SCENE_GAMEOVER = 2  #ゲームオーバー画面
+#定数
+WINDOW_H = 128
+WINDOW_W = 128
+TILE_FLOOR = (1, 0) #床パーツの指定
+#list用意
+
+#関数(TileMap(0)のタイルを取得)
+def get_tile(tile_x, tile_y):
+    return pyxel.tilemap(0).pget(tile_x, tile_y)
+
+#関数(タイルとのコリジョン判定)
+    #//は商を求める(余りは切り捨てる)
+    #8は今回のplayerが8×8ドットサイズだから
+def detect_collision(x, y, dy):
+    x1 = x // 8             #キャラx座標左端のTileMapの座標
+    y1 = y // 8             #キャラy座標上端のTileMapの座標
+    x2 = (x + 8 - 1) // 8   #キャラx座標右端のTileMapの座標
+    y2 = (y + 8 - 1) // 8   #キャラy座標下端のTileMapの座標
+    #画面下に移動している時 and キャラの下端が1ドットだかTileにめり込んでいる時
+    if dy > 0 and y % 8 == 1:
+        for xi in range(x1, x2 + 1):
+            if get_tile(xi, y1 + 1) == TILE_FLOOR:
+                return True
+    return False
+
+
+#関数(List実行)
+def update_list(list):
+    for elem in list:
+        elem.update()   #listの各要素に対してupdate()実行
+#関数(List描画)
+def draw_list(list):
+    for elem in list:
+        elem.draw()     #listの各要素に対してdraw()実行
+#関数(List更新)
+def cleanup_list(list):
+    i = 0
+    while i < len(list):        #listに要素が入っている間loops
+        elem = list[i]          #要素取得
+        if not elem.is_alive:   #死亡の場合
+            list.pop(i)         #要素削除
+        else:                   #生存の場合
+            i += 1              #スルー
+
+#■Player
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dx = 0
+        self.dy = 0
+        self.direction = 1
+        self.is_falling = False
+        self.is_alive = True
+    def update(self):
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
+            self.dx = -2
+            self.dy = 1
+            self.direction = -1
+        elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
+            self.dx = 2
+            self.dy = 1
+            self.direction = 1
+        else:
+            self.dx = 0
+            self.dy = 1
+        self.x = self.x + self.dx
+        self.y = self.y + 1
+    def draw(self):
+        #editorデータ描画(player)
+        pyxel.blt(self.x, self.y, 0, 8, 0, 8, 8, 0)
+
+class App:
+    def __init__(self):
+        #画面サイズの設定　titleはwindow枠にtext出せる
+        pyxel.init(WINDOW_W, WINDOW_H, title="Pyxel Base")
+        #editorデータ読み込み(コードと同じフォルダにある)
+        pyxel.load("my_resource06.pyxres")
+        self.score = 0
+        #画面遷移の初期化
+        self.scene = SCENE_TITLE
+        #Playerインスタンス生成
+        self.player = Player(pyxel.width / 2 -8, pyxel.height / 2 -8)
+        #実行開始 更新関数 描画関数
+        pyxel.run(self.update, self.draw)
+
+	#更新関数
+    def update(self):
+        #処理の画面分岐
+        if self.scene == SCENE_TITLE:
+            self.update_title_scene()
+        elif self.scene == SCENE_PLAY:
+            self.update_play_scene()
+        elif self.scene == SCENE_GAMEOVER:
+            self.update_gameover_scene()
+
+    #タイトル画面処理用update
+    def update_title_scene(self):
+        #ENTERでゲーム画面に遷移
+        if pyxel.btnr(pyxel.KEY_RETURN):
+#            pyxel.playm(0, loop = True)    #BGM再生
+            self.scene = SCENE_PLAY
+
+    #ゲーム画面処理用update
+    def update_play_scene(self):
+        #Player制御
+        self.player.update()
+        #list実行
+        #list更新
+
+    #ゲームオーバー画面処理用update
+    def update_gameover_scene(self):
+        #list実行
+        #list更新
+        #ENTERでタイトル画面に遷移
+        if pyxel.btnr(pyxel.KEY_RETURN):
+#            pyxel.playm(0, loop = True)         #BGM再生
+            self.score = 0
+            self.scene = SCENE_TITLE
+            #list更新
+
+	#描画関数
+    def draw(self):
+        #画面クリア 0は黒
+        pyxel.cls(0)
+        #描画の画面分岐
+        if self.scene == SCENE_TITLE:
+            self.draw_title_scene()
+        elif self.scene == SCENE_PLAY:
+            self.draw_play_scene()
+        elif self.scene == SCENE_GAMEOVER:
+            self.draw_gameover_scene()
+
+    #タイトル画面描画用update
+    def draw_title_scene(self):
+        pyxel.text(0, 20, "01234567890123456789012345678901", 7)
+
+    #ゲーム画面描画用update
+    def draw_play_scene(self):
+        #BG描画
+        pyxel.bltm(0, 0, 0, 0, 0, 160, 120, 0)
+        self.player.draw()
+
+    #ゲームオーバー画面描画用update
+    def draw_gameover_scene(self):
+        pyxel.text(0, 20, "01234567890123456789012345678901", 7)
+App()
