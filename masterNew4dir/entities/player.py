d0b8204e725759d1ad46c225d8eef3438e81d4d4
mekatama <tamatte@hotmail.com>
2025-08-05 20:06:39 +0900

    斜め攻撃

--- masterNew4dir/entities/player.py
+++ masterNew4dir/entities/player.py
@@ -17,6 +17,7 @@ class Player:
         self.x = x              # X座標
         self.y = y              # Y座標
         self.dir = 1            # 1:right -1:left
+        self.type = 0           # 0:横 1:上 2:下
         self.isBomb = False     # Bomb所持flag
         self.shot_timer = 0     # 弾発射までの残り時間
         self.hp = Player.HP     # HP
@@ -28,13 +29,19 @@ class Player:
         if pyxel.btn(pyxel.KEY_LEFT):
             self.x -= Player.MOVE_SPEED
             self.dir = -1
+            self.type = 0
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x += Player.MOVE_SPEED
             self.dir = 1
+            self.type = 0
         if pyxel.btn(pyxel.KEY_UP):
             self.y -= Player.MOVE_SPEED
+            self.type = 1
         if pyxel.btn(pyxel.KEY_DOWN):
             self.y += Player.MOVE_SPEED
+            self.type = 2
+        if pyxel.btnr(pyxel.KEY_UP) or pyxel.btnr(pyxel.KEY_DOWN):
+            self.type = 0
 
         # 弾の発射間隔timer制御
         if self.shot_timer > 0:  # 弾発射までの残り時間を減らす
@@ -42,7 +49,7 @@ class Player:
 
         # auto攻撃
         if self.shot_timer == 0:
-            BulletPlayer(self.game, self.x, self.y)
+            BulletPlayer(self.game, self.x, self.y, self.dir, self.type)
             # 次の弾発射までの残り時間を設定する
             self.shot_timer = Player.SHOT_INTERVAL
 
@@ -103,12 +110,15 @@ class Player:
 # 弾クラス
 class BulletPlayer:
     #定数
+    SHOT_SPEED_X = 4        # shot speed x
+    SHOT_SPEED_Y = 4        # shot speed y
     # 弾を初期化してゲームに登録する
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y, dir, type):
         self.game = game
         self.x = x
         self.y = y
-#        self.dir = dir
+        self.dir = dir
+        self.type = type
         self.life_time = 0  #生存時間
         self.hit_area = (2, 1, 5, 6)  # 当たり判定領域
         game.player_bullets.append(self)
@@ -123,10 +133,16 @@ class BulletPlayer:
     def update(self):
         #生存時間カウント
         self.life_time += 1
-        # 弾の座標を更新する
-        
-        self.x += 2
-#        self.y += 2
+        # 弾の座標を更新する(type 0:横 1:上 2:下)
+        if self.type == 0:
+            self.x += BulletPlayer.SHOT_SPEED_X * self.dir
+        elif self.type == 1:
+            self.x += BulletPlayer.SHOT_SPEED_X * self.dir
+            self.y -= BulletPlayer.SHOT_SPEED_Y
+        elif self.type == 2:
+            self.x += BulletPlayer.SHOT_SPEED_X * self.dir
+            self.y += BulletPlayer.SHOT_SPEED_Y
+        """
         # 弾が画面外に出たら弾リストから登録を削除する
         if (self.x <= -8 or
             self.x >= pyxel.width or
@@ -134,7 +150,7 @@ class BulletPlayer:
             self.y >= pyxel.height
         ):
             self.game.player_bullets.remove(self)
-        
+        """
     # 弾を描画する
     def draw(self):
         pyxel.blt(self.x, self.y, 0, 0, 8, 8, 8, 0)
